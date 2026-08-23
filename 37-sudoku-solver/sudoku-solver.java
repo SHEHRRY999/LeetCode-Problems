677b408e05a6331462a48f10f421e7ae95e27359
Shehryar Khurram <skbuisness999@gmail.com>
2026-08-23 11:15:14 +0500

    Time: 183 ms (78.34%) | Memory: 43.2 MB (5.60%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,71 @@
+class Solution {
+
+    public boolean isSafe(char[][] board, int row, int col, char digit) {
+
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == digit) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int j = 0; j < 9; j++) {
+            if (board[j][col] == digit) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int srow = (row / 3) * 3;
+        int scol = (col / 3) * 3;
+
+        for (int i = srow; i <= srow + 2; i++) {
+            for (int j = scol; j <= scol + 2; j++) {
+                if (board[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean helper(char[][] board, int row, int col) {
+
+        if (row == 9) {
+            return true;
+        }
+
+        int nextRow = row;
+        int nextCol = col + 1;
+
+        if (nextCol == 9) {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+
+        if (board[row][col] != '.') {
+            return helper(board, nextRow, nextCol);
+        }
+
+        for (char digit = '1'; digit <= '9'; digit++) {
+
+            if (isSafe(board, row, col, digit)) {
+                board[row][col] = digit;
+
+                if (helper(board, nextRow, nextCol)) {
+                    return true;
+                }
+            }
+
+            board[row][col] = '.';
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
+}
