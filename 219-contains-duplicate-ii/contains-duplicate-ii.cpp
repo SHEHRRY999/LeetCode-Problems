ef55cb753f462f98012c2dbb15c88964ddeb3870
Shehryar Khurram <skbuisness999@gmail.com>
2026-08-25 12:04:34 +0500

    Time: 115 ms (25.66%) | Memory: 142.2 MB (7.02%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    bool checking(vector<int>& nums, int k, int index){
-        for(int i = 0; i < nums.size(); i++){
-            if(index != i){
-                if(nums[index] == nums[i]){
-                    if(abs(index - i) <= k){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> set;
-        for(int i = 0; i < nums.size(); i++) {
-            if(set.find(nums[i]) != set.end()) {
-                int index = i;
-                if(checking(nums, k, index)){
+        unordered_map<int, int> lastIndex;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (lastIndex.find(nums[i]) != lastIndex.end())
+            {
+                if (i - lastIndex[nums[i]] <= k)
+                {
                     return true;
                 }
-            } 
-            else {
-                set.insert(nums[i]); 
             }
+            lastIndex[nums[i]] = i;  // update to the most recent index, every time
         }
+
         return false;
     }
 };
