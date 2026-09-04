15894d9017feeeea12b7330cdf2cdebbf2bcf7c8
Shehryar Khurram <skbuisness999@gmail.com>
2026-09-04 10:01:04 +0500

    Time: 4 ms (36.56%) | Memory: 43 MB (93.09%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+        Deque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') 
+            {
+                st.push(c);
+            }
+            else 
+            {
+                if (st.isEmpty()) 
+                {
+                    return false;
+                }
+                char top = st.peek();
+                if (top == '{' && c == '}' ||
+                    top == '(' && c == ')' ||
+                    top == '[' && c == ']') 
+                {
+                    st.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
